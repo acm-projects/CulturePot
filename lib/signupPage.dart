92aa6a9239c5efcfa42b225f1bafc59f81603bd0
nietztein <jackle12533.gmail.com>
2024-03-10 14:20:21 -0500

    editing signup screen

--- lib/signupPage.dart
+++ lib/signupPage.dart
@@ -10,6 +10,9 @@ class SignupPage extends StatelessWidget {
   final FirebaseAuth _auth = FirebaseAuth.instance;
   final TextEditingController _emailController = TextEditingController();
   final TextEditingController _passwordController = TextEditingController();
+  final TextEditingController _bioController = TextEditingController();
+  final TextEditingController _usernameController = TextEditingController();
+
   final formKey = GlobalKey<FormState>();
 
   SignupPage({required this.onSignedIn});
@@ -49,6 +52,37 @@ class SignupPage extends StatelessWidget {
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
+              //circular widget  to accept and show our selected file
+              Stack(
+                children: [
+                  CircleAvatar(
+                    radius: 64,
+                    backgroundImage: NetworkImage(
+                        'https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_960_720.png'),
+                  ),
+                  Positioned(
+                    bottom: -10,
+                    left: 80,
+                    child: IconButton(
+                      onPressed: () {},
+                      icon: const Icon(
+                        Icons.add_a_photo,
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+              // text input field for username
+              TextFormField(
+                controller: _usernameController,
+                obscureText: true,
+                decoration: InputDecoration(
+                  hintText: "Username",
+                  filled: true,
+                  fillColor: Colors.grey[200],
+                  border: OutlineInputBorder(),
+                ),
+              ),
               TextFormField(
                 controller: _emailController,
                 decoration: InputDecoration(
@@ -77,6 +111,16 @@ class SignupPage extends StatelessWidget {
                 validator: (value) => value != null && value.length < 6
                     ? 'Enter min 6 characters'
                     : null,
+              ),
+              TextFormField(
+                controller: _bioController,
+                obscureText: true,
+                decoration: InputDecoration(
+                  hintText: "Bio",
+                  filled: true,
+                  fillColor: Colors.grey[200],
+                  border: OutlineInputBorder(),
+                ),
               ),
               SizedBox(height: 20.0),
               Row(
