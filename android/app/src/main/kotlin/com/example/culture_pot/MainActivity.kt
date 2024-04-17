7adabdbc4549a514789695a2107ea8a5c169482a
nietztein <jackle12533.gmail.com>
2024-04-16 22:49:53 -0500

    i need to fix this merge

--- android/app/src/main/kotlin/com/example/culture_pot/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/culture_pot/MainActivity.kt
@@ -1,14 +1,6 @@
-<<<<<<< HEAD
 package com.example.culture_pot
 
 import io.flutter.embedding.android.FlutterActivity
 
 class MainActivity: FlutterActivity() {
 }
-=======
-package com.example.culture_pot
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity()
->>>>>>> origin/vidur3
